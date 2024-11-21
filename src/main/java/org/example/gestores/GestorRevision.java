2e407487e3b5a185ab0b17cc07df70003c8d0b78
Valentín <correavale2004@gmail.com>
2024-11-21 19:08:06 -0300

    Arreglo también el commit del gestor revisión.

--- src/main/java/org/example/gestores/GestorRevision.java
+++ src/main/java/org/example/gestores/GestorRevision.java
@@ -7,51 +7,16 @@ import org.example.modelos.*;
 
 public class GestorRevision {
     private ArrayList<Revision> listaRevisiones = new ArrayList<Revision>();
+    private GestorVehiculo gestorVehiculo;
+    private GestorCliente gestorCliente;
 
     public GestorRevision() {
     }
 
-    public ArrayList<Revision> getRevisiones() {
-        return listaRevisiones;
+    public GestorRevision(GestorVehiculo gestorVehiculo, GestorCliente gestorCliente) {
+        this.gestorVehiculo = gestorVehiculo;
+        this.gestorCliente = gestorCliente;
     }
-
-    public void setRevisiones(ArrayList<Revision> revisiones) {
-        this.listaRevisiones = revisiones;
-    }
-
-    public void agregarRevision(int nroRevision, Cliente cliente, Vehiculo vehiculo, Medicion medicion,
-                                DefectoRevision defecto, Oblea oblea, Date fechaAlta, String resultado,
-                                Date fechaVencimiento, Empleado supervisor, EstacionVTV estacion, Cobro pago,
-                                DefectoVisual defectoVisual){
-        Revision r = new Revision(nroRevision, cliente, vehiculo, medicion, defecto, oblea, fechaAlta, resultado,
-                 fechaVencimiento,  supervisor,  estacion,  pago, defectoVisual);
-        listaRevisiones.add(r);
-    }
-
-    public ArrayList<Revision> buscarRevisionesCliente(String numDocumento, String patente){
-        ArrayList<Revision> revisionesEncontradas= new ArrayList<>();
-        for(Revision revision:listaRevisiones) {
-            if(revision.getCliente().getNroDocumento().equals(numDocumento)){
-                revisionesEncontradas.add(revision);
-            } else if(revision.getVehiculo().getPatente().equals(patente)) {
-                revisionesEncontradas.add(revision);
-            }
-        }
-        return revisionesEncontradas;
-    }
-}
-=======
-        private ArrayList<Revision> listaRevisiones = new ArrayList<Revision>();
-        private GestorVehiculo gestorVehiculo;
-        private GestorCliente gestorCliente;
-
-        public GestorRevision() {
-        }
-
-        public GestorRevision(GestorVehiculo gestorVehiculo, GestorCliente gestorCliente) {
-            this.gestorVehiculo = gestorVehiculo;
-            this.gestorCliente = gestorCliente;
-        }
 
     public GestorVehiculo getGestorVehiculo() {
         return gestorVehiculo;
@@ -70,42 +35,41 @@ public class GestorRevision {
     }
 
     public ArrayList<Revision> getRevisiones() {
-            return listaRevisiones;
-        }
+        return listaRevisiones;
+    }
 
-        public void setRevisiones(ArrayList<Revision> revisiones) {
-            this.listaRevisiones = revisiones;
-        }
+    public void setRevisiones(ArrayList<Revision> revisiones) {
+        this.listaRevisiones = revisiones;
+    }
 
-        public int getNumeroRevision() { // Este método es empleado para generar el número de la próxima revisión que se registre.
-            return listaRevisiones.size() + 1;
-        }
+    public int getNumeroRevision() { // Este método es empleado para generar el número de la próxima revisión que se registre.
+        return listaRevisiones.size() + 1;
+    }
 
-        public void agregarRevision(int nroRevision, Cliente cliente, Vehiculo vehiculo, Medición medicion,
-                                    DefectoRevisión defecto, Oblea oblea, Date fechaAlta, String resultado,
-                                    Date fechaVencimiento, Empleado supervisor, EstacionVTV estacion, Cobro pago,
-                                    DefectoVisual defectoVisual){
-            Revision r = new Revision(nroRevision, cliente, vehiculo, medicion, defecto, oblea, fechaAlta, resultado,
-                    fechaVencimiento,  supervisor,  estacion,  pago, defectoVisual);
-            listaRevisiones.add(r);
-        }
+    public void agregarRevision(int nroRevision, Cliente cliente, Vehiculo vehiculo, Medición medicion,
+                                DefectoRevisión defecto, Oblea oblea, Date fechaAlta, String resultado,
+                                Date fechaVencimiento, Empleado supervisor, EstacionVTV estacion, Cobro pago,
+                                DefectoVisual defectoVisual){
+        Revision r = new Revision(nroRevision, cliente, vehiculo, medicion, defecto, oblea, fechaAlta, resultado,
+                fechaVencimiento,  supervisor,  estacion,  pago, defectoVisual);
+        listaRevisiones.add(r);
+    }
 
     public void agregarRevision(int nroRevision, Cliente cliente, Vehiculo vehiculo, Date fechaAlta, EstacionVTV estacion){
         Revision r = new Revision(nroRevision, cliente, vehiculo,estacion);
         listaRevisiones.add(r);
     }
 
-        //SIN IMPLEMENTAR - FALTA CLASE REVISION
-        public ArrayList<Revision> buscarRevisionesCliente(String numDocumento, String patente){
-            ArrayList<Revision> revisionesEncontradas= new ArrayList<>();
-            for(Revision revision : listaRevisiones) {
-                if(revision.getCliente().getNroDocumento().equals(numDocumento)){
-                    revisionesEncontradas.add(revision);
-                } else if(revision.getVehiculo().getPatente().equals(patente)) {
-                    revisionesEncontradas.add(revision);
-                }
+    //SIN IMPLEMENTAR - FALTA CLASE REVISION
+    public ArrayList<Revision> buscarRevisionesCliente(String numDocumento, String patente){
+        ArrayList<Revision> revisionesEncontradas= new ArrayList<>();
+        for(Revision revision : listaRevisiones) {
+            if(revision.getCliente().getNroDocumento().equals(numDocumento)){
+                revisionesEncontradas.add(revision);
+            } else if(revision.getVehiculo().getPatente().equals(patente)) {
+                revisionesEncontradas.add(revision);
             }
-            return revisionesEncontradas;
         }
+        return revisionesEncontradas;
     }
->>>>>>> Stashed changes
+}
