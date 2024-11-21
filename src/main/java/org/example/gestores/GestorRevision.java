81c077ed1f568d6184d5a869917c6214816e6ffb
Valentín <correavale2004@gmail.com>
2024-11-21 18:59:34 -0300

    Modifico clases revisión y gestor revisión para que sean funcionales al CU "Registrar revisión".

--- src/main/java/org/example/gestores/GestorRevision.java
+++ src/main/java/org/example/gestores/GestorRevision.java
@@ -40,3 +40,72 @@ public class GestorRevision {
         return revisionesEncontradas;
     }
 }
+=======
+        private ArrayList<Revision> listaRevisiones = new ArrayList<Revision>();
+        private GestorVehiculo gestorVehiculo;
+        private GestorCliente gestorCliente;
+
+        public GestorRevision() {
+        }
+
+        public GestorRevision(GestorVehiculo gestorVehiculo, GestorCliente gestorCliente) {
+            this.gestorVehiculo = gestorVehiculo;
+            this.gestorCliente = gestorCliente;
+        }
+
+    public GestorVehiculo getGestorVehiculo() {
+        return gestorVehiculo;
+    }
+
+    public void setGestorVehiculo(GestorVehiculo gestorVehiculo) {
+        this.gestorVehiculo = gestorVehiculo;
+    }
+
+    public GestorCliente getGestorCliente() {
+        return gestorCliente;
+    }
+
+    public void setGestorCliente(GestorCliente gestorCliente) {
+        this.gestorCliente = gestorCliente;
+    }
+
+    public ArrayList<Revision> getRevisiones() {
+            return listaRevisiones;
+        }
+
+        public void setRevisiones(ArrayList<Revision> revisiones) {
+            this.listaRevisiones = revisiones;
+        }
+
+        public int getNumeroRevision() { // Este método es empleado para generar el número de la próxima revisión que se registre.
+            return listaRevisiones.size() + 1;
+        }
+
+        public void agregarRevision(int nroRevision, Cliente cliente, Vehiculo vehiculo, Medición medicion,
+                                    DefectoRevisión defecto, Oblea oblea, Date fechaAlta, String resultado,
+                                    Date fechaVencimiento, Empleado supervisor, EstacionVTV estacion, Cobro pago,
+                                    DefectoVisual defectoVisual){
+            Revision r = new Revision(nroRevision, cliente, vehiculo, medicion, defecto, oblea, fechaAlta, resultado,
+                    fechaVencimiento,  supervisor,  estacion,  pago, defectoVisual);
+            listaRevisiones.add(r);
+        }
+
+    public void agregarRevision(int nroRevision, Cliente cliente, Vehiculo vehiculo, Date fechaAlta, EstacionVTV estacion){
+        Revision r = new Revision(nroRevision, cliente, vehiculo,estacion);
+        listaRevisiones.add(r);
+    }
+
+        //SIN IMPLEMENTAR - FALTA CLASE REVISION
+        public ArrayList<Revision> buscarRevisionesCliente(String numDocumento, String patente){
+            ArrayList<Revision> revisionesEncontradas= new ArrayList<>();
+            for(Revision revision : listaRevisiones) {
+                if(revision.getCliente().getNroDocumento().equals(numDocumento)){
+                    revisionesEncontradas.add(revision);
+                } else if(revision.getVehiculo().getPatente().equals(patente)) {
+                    revisionesEncontradas.add(revision);
+                }
+            }
+            return revisionesEncontradas;
+        }
+    }
+>>>>>>> Stashed changes
--- src/main/java/org/example/modelos/Revision.java
+++ src/main/java/org/example/modelos/Revision.java
@@ -127,3 +127,142 @@ public class Revision {
 
 
 }
+=======
+import java.time.LocalDate;
+
+public class Revision {
+        private int nroRevision;
+        private Cliente cliente;
+        private Vehiculo vehiculo;
+        private Medición medicion;
+        private DefectoRevisión defecto;
+        private Oblea oblea;
+        private LocalDate fechaAlta;
+        private String resultado;
+        private LocalDate fechaVencimiento;
+        private Empleado supervisor;
+        private EstacionVTV estacion;
+        private Cobro pago;
+        private DefectoVisual defectoVisual;
+
+        public Revision() {
+        }
+
+        public Revision(int nroRevision, Cliente cliente, Vehiculo vehiculo, Medición medicion, DefectoRevisión defecto,Oblea oblea, LocalDate fechaAlta, String resultado, LocalDate fechaVencimiento, Empleado supervisor, EstacionVTV estacion, Cobro pago, DefectoVisual defectoVisual) {
+            this.nroRevision = nroRevision;
+            this.cliente = cliente;
+            this.vehiculo = vehiculo;
+            this.medicion = medicion;
+            this.defecto = defecto;
+            this.oblea = oblea;
+            this.fechaAlta = fechaAlta;
+            this.resultado = resultado;
+            this.fechaVencimiento = fechaVencimiento;
+            this.supervisor = supervisor;
+            this.estacion = estacion;
+            this.pago = pago;
+            this.defectoVisual = defectoVisual;
+        }
+
+    public Revision(int nroRevision, Cliente cliente, Vehiculo vehiculo, EstacionVTV estacion) {
+        this.nroRevision = nroRevision;
+        this.cliente = cliente;
+        this.vehiculo = vehiculo;
+        this.estacion = estacion;
+        this.fechaAlta = LocalDate.now();
+    }
+
+    public int getNroRevision() {
+            return nroRevision;
+        }
+
+        public void setNroRevision(int nroRevision) {
+            this.nroRevision = nroRevision;
+        }
+        public Cliente getCliente() {
+            return cliente;
+        }
+        public void setCliente(Cliente cliente) {
+            this.cliente = cliente;
+        }
+        public Vehiculo getVehiculo() {
+            return vehiculo;
+        }
+        public void setVehiculo(Vehiculo vehiculo) {
+            this.vehiculo = vehiculo;
+        }
+        public Medición getMedicion() {
+            return medicion;
+        }
+        public void setMedicion(Medición medicion) {
+            this.medicion = medicion;
+        }
+        public DefectoRevisión getDefecto() {
+            return defecto;
+        }
+        public void setDefecto(DefectoRevisión defecto) {
+            this.defecto = defecto;
+        }
+        public Oblea getOblea() {
+            return oblea;
+        }
+        public void setOblea(DefectoRevisión defecto) {
+            this.oblea = oblea;
+        }
+        public Date getFechaAlta() {
+            return fechaAlta;
+        }
+        public void setFechaAlta(Date fechaAlta) {
+            this.fechaAlta = fechaAlta;
+        }
+        public String getResultado() {
+            return resultado;
+        }
+        public void setResultado(String resultado) {
+            this.resultado = resultado;
+        }
+        public Date getFechaVencimiento() {
+            return fechaVencimiento;
+        }
+
+        public void setFechaVencimiento(Date fechaVencimiento) {
+            this.fechaVencimiento = fechaVencimiento;
+        }
+        public Empleado getSupervisor() {
+            return supervisor;
+        }
+        public void setSupervisor(Empleado supervisor) {
+            this.supervisor = supervisor;
+        }
+        public EstacionVTV getEstacion() {
+            return estacion;
+        }
+        public void setEstacion(EstacionVTV estacion) {
+            this.estacion = estacion;
+        }
+        public DefectoVisual getDefectoVisual() {
+            return defectoVisual;
+        }
+        public void setDefectoVisual(DefectoVisual defectoVisual) {
+            this.defectoVisual = defectoVisual;
+        }
+
+        public boolean asignarOblea(Vehiculo v){
+            //esta es una funcionalidad de otro sistema, no se la implementa aquí
+            return true;
+        }
+
+        public boolean calcularResultado(Vehiculo v){
+            //esta es una funcionalidad de otro sistema, no se la implementa aquí
+            return true;
+        }
+
+        public boolean emitirInforme(Vehiculo v){
+            //esta es una funcionalidad de otro sistema, no se la implementa aquí
+            return true;
+        }
+
+
+    }
+
+>>>>>>> Stashed changes
